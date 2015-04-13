f654742b25904e5c58c694b75d5fbd761290f446
Nick Loadholtes <nick@ironboundsoftware.com>
2015-04-13 14:18:37 -0400

    wip

--- lockheed/script.js
+++ lockheed/script.js
@@ -2,6 +2,7 @@
 // we shall call "myApp") and declare our dependencies
 // (here we have just one dependency which is ngRoute, 
 // Angular's default routing service)
+//Originally from: http://www.angularjstutorial.com/2014/05/20/angularjs-basics-part-3-routing/
 angular.module('myApp',['ngRoute'])
  .config(function($routeProvider) {
    $routeProvider
@@ -11,9 +12,14 @@ angular.module('myApp',['ngRoute'])
     });
  })
  .controller('myAppController', function($scope) {
-     $scope.keyword = '';
+     $scope.keyword = null;
      var url = 'http://search.lockheedmartinjobs.com/Rss/ByKeyword/';
      console.log("testing")
      console.log($scope.keyword)
+     if ($scope.keyword) {
+         console.log("Searching for keyword:" + $scope.keyword);
+         //Search for
+         url = url + $scope.keyword;
+     }
      $scope.results = "";
  });
